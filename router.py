495a54a7832c5fc076d47200f36579c1eadde3c8
dimo nguembou idriss guerin <dimoidrissguerin@yahoo.com>
2022-05-18 13:20:09 +0200

    init commit

---
+++ router.py
@@ -0,0 +1,5 @@
+from vues.menuVue import *
+
+
+if __name__ == "__main__" :
+  showMenu()
---
+++ vues/menuVue.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def showMenu():
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack()
+
+    root.title("devops Project")
+
+    w = tk.Label(root, text="Que voulez vous faire ?", fg="red", font=("Arial Bold", 25))
+    w.pack()
+
+    button = tk.Button(frame, text="Connexion Routeur/switch", fg="blue", command=connectRouteur)
+    button.pack(side=tk.LEFT)
+
+    root.mainloop()
+
+
+def connectRouteur():
+    print("youpi cool")
+    return True
